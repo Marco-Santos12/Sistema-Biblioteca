2435c419da8e88c6f54d80806a327987f0647506
Marco-Santos12 <marco.santos28@aluno.educa.go.gov.br>
2026-09-21 18:02:15 +0000

    implementação objeto Livro e metodo Adicionar

---
+++ Biblioteca/data/LivroRepository.cs
@@ -0,0 +1,17 @@
+﻿namespace Biblioteca.Data
+{
+    public class LivroRepository
+    {
+        private static List<Livro> _tabelaLivros = new List<Livro>();
+
+        private static int proximoID = 1; // Variável auxiliar 
+
+        public void Adicionar (Livro livro) //método: adiciona livros na lista
+        {
+          livro.Id = proximoID++;
+          _tabelaLivros.Add(Livro);
+        }
+        
+
+    }
+}
---
+++ Biblioteca/models/Livro.cs
@@ -0,0 +1,12 @@
+﻿namespace Biblioteca.Models
+{
+    public class Livro
+    {
+
+            public int id(get; set;)
+
+            public string Titulo(get; set;)
+        }
+    }
+    
+
